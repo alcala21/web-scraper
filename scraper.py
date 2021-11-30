ca669ddbe31e54e7c7ad3b2c7f447e9e07210718
Carlos Alcala <alcala21@gmail.com>
2021-11-30 16:23:47 -0600

    Stage 3/5. Web Scraper.

--- scraper.py
+++ scraper.py
@@ -1,22 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
 
-class QuoteError(Exception):
+class MovieError(Exception):
     ...
 
 
 url = input("Input the URL:\n")
-
 r = requests.get(url)
-
-try:
-    if r:
-        body = r.json()
-        if 'content' in body:
-            print(body['content'])
-        else:
-            raise QuoteError
-    else:
-        raise QuoteError
-except QuoteError:
-    print("Invalid quote resource!")
+sc = r.status_code
+if sc == 200:
+    with open("source.html", "wb") as f:
+        f.write(r.content)
+    print("Content saved.")
+else:
+    print(f"The URL returned {sc}!")
