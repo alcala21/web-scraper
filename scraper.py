ecd5601ed3dd68d5d3b06eda2b830848ba702885
Carlos Alcala <alcala21@gmail.com>
2021-12-01 16:06:02 -0600

    Stage 4/5. Web Scraper.

--- scraper.py
+++ scraper.py
@@ -1,17 +1,28 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
-class MovieError(Exception):
-    ...
+def get_content(_url):
+    r = requests.get(_url)
+    return BeautifulSoup(r.content, 'html.parser') if r else None
 
 
-url = input("Input the URL:\n")
-r = requests.get(url)
-sc = r.status_code
-if sc == 200:
-    with open("source.html", "wb") as f:
-        f.write(r.content)
-    print("Content saved.")
-else:
-    print(f"The URL returned {sc}!")
+root = "https://www.nature.com"
+suf = "/nature/articles?sort=PubDate&year=2020&page=3"
+
+soup = get_content(root + suf)
+
+if soup:
+    articles = soup.find_all("article")
+    for article in articles:
+        if article.find("span", {"class": "c-meta__type"}).text == "News":
+            info = article.find("a", {"itemprop": "url"})
+            title = re.sub(r"[^\w\s]", "", info.text)
+            savename = re.sub(r"\s", "_", title) + ".txt"
+            content = get_content(root + info.get("href"))
+            body = content.find("div", {"class": "c-article-body"})
+            with open(savename, "w", encoding="utf-8") as f:
+                f.write(body.text)
+            print("Content saved.")
+
